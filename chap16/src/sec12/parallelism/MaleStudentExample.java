73a372da055d893e3ca3deb53c683555c93dbcdb
benigndeus <benigndeus@gmail.com>
2020-04-12 18:12:31 +0900

    16장 끝났다~~

---
+++ chap16/src/sec12/parallelism/MaleStudentExample.java
@@ -0,0 +1,25 @@
+package sec12.parallelism;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaleStudentExample {
+    public static void main(String[] args) {
+        List<Student> totalList = Arrays.asList(
+            new Student("홍길동", 10, Student.Sex.MALE),
+            new Student("김수애", 6, Student.Sex.FEMALE),
+            new Student("신용권", 10, Student.Sex.MALE),
+            new Student("박수미", 6, Student.Sex.FEMALE)
+        );
+
+        MaleStudent maleStudent = totalList.parallelStream()
+            .filter(s->s.getSex()==Student.Sex.MALE)
+            .collect(
+                () -> new MaleStudent(), 
+                (r,t) -> r.accumulate(t),
+                (r1,r2) -> r1.combine(r2));
+
+        maleStudent.getList().stream()
+            .forEach(s->System.out.println(s.getName()));
+    }
+}
